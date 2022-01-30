6d2dae27e79a0dd05a88ba69e9f0badfade40e90
nkhaldi <mnk80898@gmail.com>
2022-01-30 17:59:26 +0300

    modofied hello

--- hello.go
+++ hello.go
@@ -3,5 +3,8 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello, World!")
+    fmt.Println("What is your name?")
+    var name string
+    fmt.Scanln(&name)
+    fmt.Println("Hello,", name + "!")
 }
