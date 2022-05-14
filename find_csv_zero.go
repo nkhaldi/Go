e2a772a4313b477526d56ac9cfb58a885ff20609
nkhaldi <mnk80898@gmail.com>
2022-05-14 20:11:48 +0300

    added new stepik-task

---
+++ find_csv_zero.go
@@ -0,0 +1,36 @@
+/*
+   В тестовом файле "task.data", содержится длинный ряд чисел,
+   разделенных символом ";". Требуется найти, на какой позиции
+   находится число 0 и указать её в качестве ответа.
+   Требуется вывести именно позицию числа, а не индекс.
+*/
+
+package main
+
+import (
+	"os"
+	"fmt"
+	"bufio"
+)
+
+func main() {
+	file, err := os.Open("tests/task.data")
+	defer file.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	r := bufio.NewReader(file)
+	var pos int = 1
+	for {
+		str, err := r.ReadString(';')
+		if err != nil {
+			panic(err)
+		}
+		if str == "0;" {
+			fmt.Println(pos)
+			break
+		}
+		pos++
+	}
+}
