769284a8a408c7d1ec574199c1ff9fb7404a7ead
nkhaldi <mnk80898@gmail.com>
2022-05-20 01:23:59 +0300

    adde new script with date parsing

---
+++ dateDiff.go
@@ -0,0 +1,36 @@
+/*На стандартный ввод подается строковое представление двух дат, разделенных запятой (формат данных смотрите в примере).
+Необходимо преобразовать полученные данные в тип Time, а затем вывести продолжительность периода между меньшей и большей датами.*/
+package main
+
+import (
+	"io"
+	"os"
+	"fmt"
+	"time"
+	"bufio"
+	"strings"
+)
+
+func main() {
+	rd, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil && err != io.EOF {
+		panic(err)
+	}
+
+	dates := strings.Split(strings.Trim(rd, "\n"), ",")
+    date1, err := time.Parse("02.01.2006 15:04:05", dates[0])
+	if err != nil {
+		panic(err)
+	}
+	date2, err := time.Parse("02.01.2006 15:04:05", dates[1])
+	if err != nil {
+		panic(err)
+	}
+
+	if date2.Before(date1) {
+		date1, date2 = date2, date1
+	}
+
+	diff := date2.Sub(date1)
+	fmt.Println(diff)
+}
