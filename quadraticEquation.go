5f0426a96c3e73d1249011129081e26e9c104dbc
nkhaldi <mnk80898@gmail.com>
2022-07-12 17:56:49 +0300

    started solving Equation

--- quadraticEquation.go
+++ quadraticEquation.go
@@ -1,34 +1,47 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "math"
+)
 
 func main() {
     for {
     a, b, c := getParameters()
     printEquation(a, b, c)
-}
+        fmt.Println("\n")
+    }
     //x1, x2 := solveEquation(a, b, c)
     //fmt.Printf("x1 = %.2f, x2 = %.2f\n", x1, x2)
 }
 
 
-func solveEquation(a, b, c float32) (float32, float32) {
-    var x1, x2 float32
+func solveEquation(a, b, c float64) (float64, float64) {
+    var x1, x2 float64
 
-    x1 = 123.45
-    x2 = 321.54
+    if (a != 0) {
+        var discr, sqrtD float64
+        discr = b*b - 4*a*c
+        if (discr >= 0) {
+            sqrtD = math.Sqrt(discr)
+            x1 = (-b - sqrtD) / (2 * a)
+            x2 = (-b + sqrtD) / (2 * a)
+        }
+    } else if (b != 0) {
+        x1 = -c / b
+    }
     return x1, x2
 }
 
 
-func printEquation(a, b, c float32) {
+func printEquation(a, b, c float64) {
     if (a != 0) {
         if (a == -1) {
             fmt.Print("-")
         } else if (a != 1) {
-            fmt.Print(a)
+            fmt.Printf("%.2f", a)
         }
-        fmt.Print("*x^2")
+        fmt.Print("x^2")
     }
     if (b != 0) {
         if (a != 0) {
@@ -38,16 +51,16 @@ func printEquation(a, b, c float32) {
                 fmt.Print(" - ")
             }
             if (b*b != 1) {
-                fmt.Printf("%.2f", b)
+                fmt.Printf("%.2f*", b)
             }
         } else {
             if (b == -1) {
                 fmt.Print("-")
             } else if (b != 1) {
-                fmt.Printf("%.2f", b)
+                fmt.Printf("%.2f*", b)
             }
         }
-        fmt.Print("*x");
+        fmt.Print("x");
     }
     if (c != 0) {
         if (a != 0 || b != 0) {
@@ -67,8 +80,8 @@ func printEquation(a, b, c float32) {
 }
 
 
-func getParameters() (float32, float32, float32) {
-    var a, b, c float32
+func getParameters() (float64, float64, float64) {
+    var a, b, c float64
     fmt.Println("Enter parameters of equation:")
     fmt.Println("ax^2 + bx + c = 0")
     fmt.Print("a = ");
